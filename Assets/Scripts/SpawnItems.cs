3675c632cce1cc6cfaaf2b093ce1f25625181e76
Nikita Chobotov <chobotov@bk.ru>
2021-08-05 00:14:15 +0300

    Поправил код спавна кружков

---
+++ Assets/Scripts/SpawnItems.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace TapSwap
+{
+    public class SpawnItems : MonoBehaviour
+    {
+        [SerializeField] private DropItem[] _itemsToSpawn;
+        [SerializeField] private Vector3[] _spawnXY;
+
+        public void Spawn()
+        {
+            var itemIndex = Random.Range(0, _itemsToSpawn.Length);
+            var posIndex = Random.Range(0, _spawnXY.Length);
+            var pos = new Vector3(_spawnXY[posIndex].x, transform.position.y);
+
+            Instantiate(_itemsToSpawn[itemIndex], pos, Quaternion.identity);
+        }
+    }
+}
